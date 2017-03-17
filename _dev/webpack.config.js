3b49f7dc1238d70567c1c1a5622a1788806ab04a
Peter Höffling <p.hoeffling@gmail.com>
2017-03-17 07:06:24 +0100

    [chore] Add linting to watch mode.

--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -1,41 +1,52 @@
 const path = require('path')
+const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const LiveReloadPlugin = require('webpack-livereload-plugin')
 
 module.exports = {
-    devtool: 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        filename: 'remace-bundle.js',
-        path: path.resolve(__dirname, '..', 'webresources')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader'
-                })
-            },
-            {
-                exclude: [
-                    /\.js$/,
-                    /\.css$/
-                ],
-                loader: 'file-loader',
-                options: {
-                    name: 'static/[name].[hash:8].[ext]'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin('remace-bundle.css'),
-        new LiveReloadPlugin()
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'remace-bundle.js',
+    path: path.resolve(__dirname, '..', 'webresources')
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        exclude: [
+          /\.js$/,
+          /\.css$/
+        ],
+        loader: 'file-loader',
+        options: {
+          name: 'static/[name].[hash:8].[ext]'
+        }
+      }
     ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new ExtractTextPlugin('remace-bundle.css'),
+    new LiveReloadPlugin()
+  ]
 }
